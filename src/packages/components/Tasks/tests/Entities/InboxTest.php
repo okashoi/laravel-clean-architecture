871c44b4719b7395917c75f411c6978d618b5147
Okada Shohei <okashoicircus0@gmail.com>
2019-06-19 01:41:35 +0900

    Add test class for Inbox

---
+++ src/packages/components/Tasks/tests/Entities/InboxTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Tests\Components\Tasks\Entities;
+
+use PHPUnit\Framework\TestCase;
+use DateTime;
+use MyApp\Components\Tasks\Entities\Id;
+use MyApp\Components\Tasks\Entities\Name;
+use MyApp\Components\Tasks\Entities\Inbox;
+use MyApp\Components\Tasks\Entities\EstimatedTime;
+use MyApp\Components\Tasks\Entities\StartDate;
+use MyApp\Components\Tasks\Entities\Scheduled;
+
+class InboxTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function 「Inbox」は「見積もり時間」と「着手日」を設定すると「Scheduled」になること()
+    {
+        $id = new Id(1);
+        $name = new Name('test');
+        $inbox = new Inbox($id, $name);
+
+        $estimatedTime = new EstimatedTime(1, 0);
+        $inbox->estimateTime($estimatedTime);
+
+        $startDate = new StartDate(new DateTime('tomorrow'));
+
+        $scheduled = $inbox->convertToScheduled($startDate);
+
+        $this->assertTrue($scheduled instanceof Scheduled);
+    }
+
+    /**
+     * @test
+     * @doesNotPerformAssertions
+     */
+    public function 「Inbox」に対して「着手日」が未設定のままでも「見積もり時間」を設定できること()
+    {
+        $id = new Id(1);
+        $name = new Name('test');
+        $inbox = new Inbox($id, $name);
+
+        $estimatedTime = new EstimatedTime(1, 0);
+        $inbox->estimateTime($estimatedTime);
+    }
+
+    /**
+     * @test
+     * @expectedException \MyApp\Components\Tasks\Entities\EstimatedTimeNotSet
+     */
+    public function 「Inbox」に対して「見積もり時間」が未設定のまま「着手日」を設定できないこと()
+    {
+        $id = new Id(1);
+        $name = new Name('test');
+        $inbox = new Inbox($id, $name);
+
+        $startDate = new StartDate(new DateTime('tomorrow'));
+
+        $inbox->convertToScheduled($startDate);
+    }
+
+    /**
+     * @test
+     * @expectedException \MyApp\Components\Tasks\Entities\InvalidArgumentException
+     */
+    public function 「着手日」に過去の日付を能動的に設定できないこと()
+    {
+        $id = new Id(1);
+        $name = new Name('test');
+        $inbox = new Inbox($id, $name);
+
+        $estimatedTime = new EstimatedTime(1, 0);
+        $inbox->estimateTime($estimatedTime);
+
+        $startDate = new StartDate(new DateTime('yesterday'));
+
+        $inbox->convertToScheduled($startDate);
+    }
+}
